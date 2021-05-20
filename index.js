0ca48b55dfe08b37767c3b55f693c497dc99c70f
Anyanwu Maureen <maureenanyanwu519@gmail.com>
2021-05-20 16:09:36 +0100

    added file

---
+++ index.js
@@ -0,0 +1,94 @@
+function convertFahrToCelsius(num){
+
+	if(Array.isArray(num)==true){
+
+		return `[${num}] is not a valid number but a/an array`
+	}
+
+	else if(typeof(num)=="boolean"){
+		return `${num} is not a valid number but a/an ${typeof(num)}`;
+	}
+
+	else if(isNaN(num)==true){
+		var inputType= typeof(num);
+		if(inputType=="object"){
+			var objectParams= JSON.stringify(num);
+			return `${objectParams} is not a valid number but a/an ${inputType}`;
+		}
+
+		else{
+			return `${num} is not a valid number but a/an ${inputType}`;
+		}
+		
+	} 
+
+	else{
+		var x= Number(num)-32;
+		var y= 0.55555556 * x;
+		return y.toFixed(4)
+	}
+}
+
+convertFahrToCelsius(0)
+convertFahrToCelsius([1,2,3])
+convertFahrToCelsius({"temp":0})
+
+function checkYuGiOh(n){
+	
+	if(isNaN(n)==true){
+		console.log(`invalid parameter: "${n}"`)
+	}
+
+	else if(typeof(n)=="boolean"){
+		console.log(`invalid parameter: "${n}"`)
+	}
+
+	else{
+		var arr=[];
+		let num=Number(n);
+		for(var i=1; i<num+1; i++){
+			if(i%2==0&&i%3==0&&i%5==0){
+				arr.push("yu-gi-oh")
+			}
+			else if(i%2==0&&i%3==0&&i%5!=0){
+				arr.push("yu-gi")
+			}
+			else if(i%2==0&&i%3!=0&&i%5==0){
+				arr.push("yu-oh")
+			}
+			else if(i%2!=0&&i%3==0&&i%5==0){
+				arr.push("gi-oh")
+			}
+			else if(i%2==0&&i%3!=0&&i%5!=0){
+				arr.push("yu")
+			}
+			else if(i%2!=0&&i%3==0&&i%5!=0){
+				arr.push("gi")
+			}
+			else if(i%2!=0&&i%3!=0&&i%5==0){
+				arr.push("oh")
+			}
+			else{
+				arr.push(i);
+			}
+		}
+		console.log(arr);
+	}
+
+}
+
+checkYuGiOh(10)
+checkYuGiOh("5")
+checkYuGiOh("fizzbuzz is meh")
+
+
+
+
+
+
+
+
+
+
+
+
